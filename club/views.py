8f099f80b70c3c5b0307e603a6d9cd7e40c1e296
youngrend <youngrend@gmail.com>
2021-06-12 17:08:34 -0700

    Assignment 5 Resource View

--- club/views.py
+++ club/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Meeting, Meeting_Minutes, Resource, Event
 
 # Create your views here.
 def index(request):
     return render(request, 'club/index.html')
+
+def resources(request):
+    resource_list=Resource.objects.all()
+    return render(request, 'club/resources.html', {'resource_list': resource_list})
+
